64886e0bee6debf7cbc5ee4f85afd43253515712
Andy Stef <Andy.stef.2512@gmail.com>
2017-02-06 12:56:40 +0200

    initial commit

---
+++ AndyPhoto/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AndyPhoto
+//
+//  Created by Andy Stef on 2/6/17.
+//  Copyright © 2017 Andy Stef. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
